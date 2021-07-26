ee61b30567d694c3ec3f758177cd56371c9a4665
Maximiliano Candia <maxi_kndia@hotmail.com>
2021-07-26 00:31:33 -0300

    App lista de tareas por consola

---
+++ 02-node-avanzado/app-lista-tareas/app.js
@@ -0,0 +1,9 @@
+const colors  = require('colors');
+const { mostrarMenu } = require('./helpers/mensajes');
+
+console.clear();
+
+const main = async() => {console.log('Hola Mundo'.green)};
+
+mostrarMenu();
+main();
