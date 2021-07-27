485b57287287a553409312f76f21bd42fe2f2ac9
Maximiliano Candia <maxi_kndia@hotmail.com>
2021-07-26 21:46:56 -0300

    agregando logica de manejo de tareas por hacer

--- 02-node-avanzado/app-lista-tareas/app.js
+++ 02-node-avanzado/app-lista-tareas/app.js
@@ -1,6 +1,9 @@
 const colors  = require('colors');
 const { inquirerMenu, pausa } = require('./helpers/inquirer');
 //const { mostrarMenu, pausa } = require('./helpers/mensajes');
+const Tarea = require('./models/tarea');
+const Tareas = require('./models/tareas');
+
 
 console.clear();
 
@@ -8,8 +11,16 @@ const main = async() => {
     
     let opt = '';
     do {
-       opt = await inquirerMenu();
-        console.log({ opt });
+        //opt = await inquirerMenu();
+        //console.log({ opt });
+        const tareas = new Tareas();
+        const tarea = new Tarea('Comprar comida');
+        
+        tareas._listado[tarea.id] =  tarea;
+        
+        console.log(tareas);
+
+
 
         await pausa();
 
